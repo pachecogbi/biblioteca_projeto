efcbc8cd3d903dc9130371262c59cc67f8994111
Pacheco <pachecogbi@outlook.com>
2022-10-06 17:33:46 -0300

    breeze

---
+++ app/Mail/LivroMail.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class LivroMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct(
+        public string $nomeLivro,
+        public string $nomeAutor
+    )
+    {
+
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->markdown('emails.livro-mail');
+    }
+}
---
+++ resources/views/emails/livro-mail.blade.php
@@ -0,0 +1,7 @@
+@component('mail::message')
+
+# Novo livro adicionado
+
+    O Livro "{{ $nomeLivro }}" do autor "{{ $nomeAutor }}" foi adicionado a sua biblioteca!
+
+@endcomponent
